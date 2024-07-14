3d490cab39144b18814c70b8ee75056b47957021
Ayushii6 <aj801852@gmail.com>
2024-07-14 20:29:04 +0530

    Add files via upload

--- cliBasedChatApp/sender.py
+++ cliBasedChatApp/sender.py
@@ -1,1 +1,17 @@
+import socket as sk
 
+s = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+target_ip= "127.0.0.1"
+port_no = 2525
+target_address=(target_ip, port_no)
+
+
+quiet = True
+while quiet:
+    message = input("please enter the message: ")
+    message_encrypt = message.encode("ascii")
+    s.sendto(message_encrypt,target_address)
+
+user_input = input("do you want to quit it press Y\y : ")
+if user_input == 'y' or user_input == 'Y':
+    quiet = False
